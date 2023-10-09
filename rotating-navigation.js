2d01701d6dc1c39343bb225b18128706fabed7f0
Alaina Harrison <amvoce1221@gmail.com>
2023-10-09 15:10:34 -0700

    added js to add classes for showing and hiding nav

--- rotating-navigation.js
+++ rotating-navigation.js
@@ -3,3 +3,10 @@ and when the menu icon is clicked it will rotate the whole website and brings in
 in the nav items on the side of the website that rotated. The Menu button will 
 also change depending on what state it is in */
 
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const container = document.querySelector('.container');
+
+open.addEventListener('click', () => container.classList.add('show-nav'));
+
+close.addEventListener('click', () => container.classList.remove('show-nav'));
