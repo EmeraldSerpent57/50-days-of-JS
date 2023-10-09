45154f58381f00e07989f86720ebd9e030faed6f
Alaina Harrison <amvoce1221@gmail.com>
2023-10-09 13:05:17 -0700

    added HTML structure and components

---
+++ rotating-navigation.js
@@ -0,0 +1,5 @@
+/* This project will have a menu item on any type of website that needs a menu, 
+and when the menu icon is clicked it will rotate the whole website and brings in 
+in the nav items on the side of the website that rotated. The Menu button will 
+also change depending on what state it is in */
+
